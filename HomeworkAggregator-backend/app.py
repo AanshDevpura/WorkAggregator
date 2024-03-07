7d1564d51bc59e4d0cf7f29032bd07dbce2a24f8
19713 <kevinchensunshine@gmail.com>
2024-03-07 14:32:55 -0600

    Updating frontend to look a bit nicer, wrong the API requirement guidelines, started work on authorization

--- HomeworkAggregator-backend/app.py
+++ HomeworkAggregator-backend/app.py
@@ -5,7 +5,24 @@ import os
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+
+#Auth Configs
+app.config['OAUTH2_PROVIDERS'] = {
+    # GitHub OAuth 2.0 documentation:
+    # https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+    'github': {
+        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
+}
 
 # Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = (
