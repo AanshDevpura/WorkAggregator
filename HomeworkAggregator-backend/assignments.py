d90466697f38b36f91caffbde2717286149ef47a
Aansh Devpura <adevpura@wirelessprv-10-192-238-15.near.illinois.edu>
2024-04-14 16:14:49 -0500

    Got rid of unecessary timezone stuff and added something to show it is always in ct

--- HomeworkAggregator-backend/assignments.py
+++ HomeworkAggregator-backend/assignments.py
@@ -1,6 +1,5 @@
 import requests
 from datetime import datetime
-from tzlocal import get_localzone
 import pytz
 import json
 import asyncio
@@ -35,8 +34,8 @@ async def get_canvas_assignments(access_token):
     canvas_assignments_url = "https://canvas.illinois.edu/api/v1//users/self/todo"
     response = requests.get(canvas_assignments_url, headers=headers)
 
-    # Get current date in local timezone
-    now = datetime.now(get_localzone())
+    # Get current date and time
+    now = datetime.now(pytz.utc)
     
     # Convert assignments to a list of Assignment object
     assignments_list = []
@@ -45,14 +44,12 @@ async def get_canvas_assignments(access_token):
 
         for submission in canvas_assignments:
             assignment = submission["assignment"]
-            due_date = datetime.strptime(assignment["due_at"], "%Y-%m-%dT%H:%M:%SZ")
-            due_date_utc = due_date.replace(tzinfo=pytz.utc)
-            due_date_local = due_date_utc.astimezone(get_localzone())
-            if due_date_local > now and assignment['submission_types']!= ['none']:
+            due_date = datetime.strptime(assignment["due_at"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.utc)
+            if due_date > now and assignment['submission_types']!= ['none']:
                 assignment_data = Assignment(
                     course_name=submission["context_name"],
                     assignment_name=assignment["name"],
-                    due_date=due_date_local,
+                    due_date=due_date,
                     source = "Canvas"
                 )
                 assignments_list.append(assignment_data)
@@ -72,23 +69,19 @@ async def get_moodle_assignments(access_token):
     response = requests.post(moodle_assignments_url, data=params)
     moodle_assignments = response.json()
 
-    # Get local timezone
-    local_tz = get_localzone()
-
-    # Get current date in local timezone
-    now = datetime.now(local_tz)
+    # Get current date and time
+    now = datetime.now(pytz.utc)
 
     # Convert assignments to a list of Assignment object
     assignments_list = []
     if 'events' in moodle_assignments:
         for event in moodle_assignments['events']:
-            due_date_utc = datetime.fromtimestamp(event['timestart'] + event['timeduration'], tz=pytz.utc)
-            due_date_local = due_date_utc.astimezone(local_tz)
-            if due_date_local > now and event['action']['name']!= 'View' and event['action']['actionable']== True:
+            due_date = datetime.fromtimestamp(event['timestart'] + event['timeduration'], tz=pytz.utc)
+            if due_date > now and event['action']['name']!= 'View' and event['action']['actionable']== True:
                 assignment_data = Assignment(
                     course_name=event['course']['fullname'],
                     assignment_name=event['name'],
-                    due_date=due_date_local,
+                    due_date=due_date,
                     source = "Moodle"
 
                 )
@@ -108,22 +101,18 @@ async def get_gradescope_assignments(email, password, is_instructor):
     # Fetch assignments from the calendar
     gradescope_assignments = calendar.assignments_all
 
-    # Get local timezone
-    local_tz = get_localzone()
-    
-    # Get current date in local timezone
-    now = datetime.now(local_tz)
+    # Get current date and time
+    now = datetime.now(pytz.utc)
 
     # Convert assignments to a list of Assignment objects
     assignments_list = []
     for assignment_name, assignment_details in gradescope_assignments.items():
         due_date = assignment_details.close_date
-        due_date_local = due_date.astimezone(local_tz)
-        if due_date_local > now and assignment_details.status != 'Submitted':
+        if due_date > now and assignment_details.status != 'Submitted':
             assignment_data = Assignment(
                 course_name=assignment_details.course.name,
                 assignment_name=assignment_details.name,
-                due_date=due_date_local,
+                due_date=due_date,
                 source = "Gradescope"
             )
             assignments_list.append(assignment_data)
